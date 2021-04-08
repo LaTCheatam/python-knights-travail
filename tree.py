ef7d2454d5fc5a37ff8e39fcc885e6b35409a751
Heba <hkasaby83@gmail.com>
2021-04-08 18:32:26 -0500

    RecursionError

--- tree.py
+++ tree.py
@@ -18,12 +18,12 @@ class Node:
     def add_child(self, node):
         if not node in self._children:
             self._children.append(node)
-            node._parent = self
+            node.parent = self   # = node.parent(self)
 
     def remove_child(self, node):
         if node in self._children:
-            self._children.pop(node)
-            node._parent = None 
+            self._children.remove(node)   
+            node.parent = None   # = node.parent(None)
 
     @property
     def parent(self):
@@ -31,6 +31,22 @@ class Node:
 
     @parent.setter
     def parent(self, node):
+        if self._parent is node:
+            return
+        if self._parent:
+            self._parent.remove_child(self)
+        self._parent = node
         if node is not None:
-            self._parent = node
             node.add_child(self)
+
+
+
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+node3.parent = node2
+
+print(node1.children)
+print(node2.children)
