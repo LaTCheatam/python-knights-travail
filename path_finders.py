423f0cd188cbdcb67685919bf5e87b5b47d2bf69
LaTCheatam <obsidyenmoon@gmail.com>
2021-04-09 10:32:17 -0700

    step: create new_move_positions

--- path_finders.py
+++ path_finders.py
@@ -2,6 +2,18 @@ from .tree import Node
 
 class KnightPathFinder:
     def __init__(self, start):
-        self._start = start
-        self._root = root
-    
+        self._root = Node(start)
+        self._considered_positions = set(start)
+        
+    def get_valid_moves(self, pos):
+        x, y = pos
+        if x > 7 or x < 0 or y > 7 or y < 0:
+           return "You have fallen off the board. Try Again!"
+
+        knight-position = []
+        knight-moves = (
+            (2, 1), (2, -1),
+            (-2, 1), (-2, -1),
+            (1, 2), (1, -2),
+            (-1, 2), (-1, -2))
+       
--- tree.py
+++ tree.py
@@ -6,6 +6,7 @@ class Node:
         self._value = value
         self._parent = None
         self._children = []
+        
 
     @property
     def value(self):
